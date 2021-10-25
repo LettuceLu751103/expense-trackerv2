25b5f14ca4d42b84b07f409adede402e743d3d35
lettuce <lettuce.lu@lettucelu.com>
2021-10-25 13:28:23 +0800

    project init

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const app = express()
+const exphbs = require('express-handlebars')
+
+// setting template engine
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+
+app.listen(3000, () => {
+  console.log('This is http server running on http://localhost:3000')
+})
