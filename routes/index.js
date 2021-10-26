afdae7d30efe2e5bc2456106d0b13e71e6fc71a5
lettuce <lettuce.lu@lettucelu.com>
2021-10-26 22:43:43 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,13 @@ const router = express.Router()
 
 const home = require('./modules/home')
 const todo = require('./modules/todo')
-
+const user = require('./modules/user')
 
 router.use('/todo', todo)
-router.use('/', home)
 
+router.use('/user', user)
+
+router.use('/', home)
 
 
 module.exports = router
