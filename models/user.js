985e3aaf2a2cdf296fca32b3486ec867ff0c5026
lettuce <lettuce.lu@lettucelu.com>
2021-10-27 12:25:31 +0800

    feat: add user registration feature

--- models/user.js
+++ models/user.js
@@ -15,7 +15,7 @@ const userSchema = mongoose.Schema({
   },
   createdAt: {
     type: Date,
-    default: Data.now
+    default: Date.now
   },
   activated: {
     type: Boolean,
--- routes/modules/user.js
+++ routes/modules/user.js
@@ -2,12 +2,14 @@ const express = require('express')
 
 const router = express.Router()
 
+const User = require('../../models/user')
+
 router.get('/login', (req, res) => {
   res.render('login')
 })
 
 router.post('/login', (req, res) => {
-  res.redirect('/')
+
 })
 
 router.get('/register', (req, res) => {
@@ -15,7 +17,43 @@ router.get('/register', (req, res) => {
 })
 
 router.post('/register', (req, res) => {
-  res.redirect('/user/login')
+
+  const { name, email, password, confirmPassword } = req.body
+  console.log(`name : ${name}`)
+  console.log(`email : ${email}`)
+  console.log(`password : ${password}`)
+  console.log(`confirmPassword : ${confirmPassword}`)
+
+  User.findOne({ email })
+    .then(user => {
+      if (user) {
+        console.log(user)
+        console.log('email already existed!')
+        // res.render('register', { name, email, password, confirmPassword })
+        res.render('register', {
+          name,
+          email,
+          password,
+          confirmPassword
+        })
+      } else {
+        console.log('data is not exist, prepare to save to db')
+        return User.create({
+          name: name,
+          email: email,
+          password: password,
+        })
+          .then(() => {
+            console.log('data already save to db!!!')
+            res.redirect('/')
+          })
+          .catch(error => {
+            console.log(error)
+          })
+      }
+    })
+
+
 })
 
 module.exports = router
